c89dd62b7c514bb174f46143ac4531e61a38dd7b
NathanCerne <90366735+NathanCerne@users.noreply.github.com>
2024-08-02 17:47:20 -0500

    Project2.py
    added descriptor comments

--- Project2.py
+++ Project2.py
@@ -3,6 +3,7 @@
 class Address(object):
     """Store info on student's address"""
 
+    """Create the address object"""
     def __init__(self, streetName, houseNumber, apartmentNumber, city, state, country):
         self.streetName = streetName
         self.houseNumber = houseNumber
@@ -11,6 +12,7 @@ class Address(object):
         self.state = state
         self.country = country
 
+    """function for printing address"""
     def display(self):
         print("Address:", self.houseNumber, self.streetName, ", apt", self.apartmentNumber, self.city, self.state, self.country)
 
@@ -19,13 +21,16 @@ class Student(object):
     email = ""
     name = ""
 
+    """Create the student object"""
     def __init__(self, studentID):
         self.studentID = studentID
         self.address = Address("", 0, "", "", "", "")
 
+    """create an address object to the student object"""
     def addAddress(self, streetName, houseNumber, apartmentNumber, city, state, country):
         self.address = Address(self, streetName, houseNumber, apartmentNumber, city, state, country)
 
+    """print student object"""
     def display(self):
         print(self.name, "has student id:", self.studentID, ", eamil:", self.email)
         self.address.display()
